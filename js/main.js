0cd9053c9bfa6bb0a50cf6ea6d98f1dbe2b76c55
EkoSusant <ekcos38@yahoo.com>
2023-08-04 12:01:48 +0700

    audia add

--- js/main.js
+++ js/main.js
@@ -15,6 +15,16 @@ $(window).on("load", function () {
     setTimeout(slideShow, 5000);
   }
   slideShow();
+
+  //audio
+  $(".fa-music").click(function () {
+    if ($(this).hasClass("pause")) {
+      $("#myAudio")[0].play();
+    } else {
+      $("#myAudio")[0].pause();
+    }
+    $(this).toggleClass("pause");
+  });
 });
 
 $(document).ready(function () {
